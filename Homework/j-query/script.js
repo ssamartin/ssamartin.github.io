06572408c8201d5ef6571620ff62c41dbf838add
ssamartin <59620514+ssamartin@users.noreply.github.com>
2020-01-30 08:49:36 -0800

    hw5

---
+++ Homework/j-query/script.js
@@ -0,0 +1,14 @@
+$('.box_1').click(function(){
+  b1 = $('.box_1');
+  b1.addClass('display_none');
+});
+
+$('.box_2').click(function(){
+  b2 = $('.box_2');
+  b2.addClass('color_blue');
+});
+
+$('.box_3').click(function(){
+  b3 = $('.box_3');
+  b3.addClass('bigger');
+});
