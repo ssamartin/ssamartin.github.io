01d35b21268f2ea03a63548a8a8afd15cdf02aaa
ssamartin <59620514+ssamartin@users.noreply.github.com>
2020-02-18 10:17:26 -0800

    updates

--- Homework/j-query/script.js
+++ Homework/j-query/script.js
@@ -6,6 +6,7 @@ $('.box_1').click(function(){
 $('.box_2').click(function(){
   b2 = $('.box_2');
   b2.addClass('color_blue');
+  b1.removeClass('display_none');
 });
 
 $('.box_3').click(function(){
